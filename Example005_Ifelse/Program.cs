91f471760b8d9f20a0e37a392179f1814339fee8
1 1 <bobra2@ya.ru>
2022-08-17 11:02:12 +0300

    else example added

---
+++ Example005_Ifelse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("insert username ");
+string username=Console.ReadLine();
+
+if (username.ToLower()=="masha")
+{
+    Console.WriteLine("Masha, Finally!");
+
+}
+else
+
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
