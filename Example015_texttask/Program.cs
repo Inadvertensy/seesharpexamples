a854d5bbacbb8d6e4200487c649bfe7eb2aef2f7
1 1 <bobra2@ya.ru>
2022-08-28 13:22:35 +0300

    task 15 modified

--- Example015_texttask/Program.cs
+++ Example015_texttask/Program.cs
@@ -30,3 +30,7 @@ string Replace(string text, char oldValue, char newValue)
 }
 string newText=Replace(text, ' ', '|');
 Console.WriteLine(newText);
+Console.WriteLine();
+newText=Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
