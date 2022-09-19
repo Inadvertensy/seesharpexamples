e936ccb1140760e5a9a2371bb93659616285b19b
1 1 <bobra2@ya.ru>
2022-09-19 18:26:11 +0300

    tasks added

---
+++ Example022_WorkWithFolders/Program.cs
@@ -0,0 +1,33 @@
+﻿// string path="C:/Users/inadv/Desktop/AI/Знакомство с сишарп/Programming/Examples/Example022_WorkWithFolders";
+// DirectoryInfo di=new DirectoryInfo(path);
+// Console.WriteLine(di.CreationTime);
+
+// FileInfo[] fi=di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     Console.WriteLine(fi[i].Name);
+// }
+// // // // // // // // // // // // // // // // // // // // //
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = "C:/Users/inadv/Desktop/AI/Знакомство с сишарп/Programming/Examples/Example022_WorkWithFolders";
+
+CatalogInfo(path);
